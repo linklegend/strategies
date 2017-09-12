4429167f91e61dc3971839186f0da9c3429445fd
linklegend <link_leagend@hotmail.com>
2017-09-12 16:12:17 +1000

    starting

---
+++ Automail.java
@@ -0,0 +1,27 @@
+package strategies;
+
+import automail.IMailDelivery;
+import automail.Robot;
+
+public class Automail {
+	      
+    public Robot robot;
+    public IMailPool mailPool;
+    
+    public Automail(IMailDelivery delivery) {
+    	// Swap between simple provided strategies and your strategies here
+    	    	/*hi*/
+    	/*how*/
+    	/** Initialize the MailPool */
+    	mailPool = new MailPool();
+    	
+        /** Initialize the RobotAction */
+    	// IRobotBehaviour robotBehaviour = new SimpleRobotBehaviour();
+    	IRobotBehaviour robotBehaviour = new SmartRobotBehaviour();
+    	    	
+    	/** Initialize robot */
+    	robot = new Robot(robotBehaviour, delivery, mailPool);
+    	
+    }
+    
+}
